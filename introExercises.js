6258190f1f77bb316e0463d9bfee41465ba17248
david ortega <david.ortega.dev@gmail.com>
2019-11-26 09:32:03 -0600

    morning exercise and intro to js playground

---
+++ introExercises.js
@@ -0,0 +1,48 @@
+var littleMermaid = 3;
+var brotherBear = 5;
+var hercules = 1;
+var dailyRental = 3.00;
+
+console.log(dailyRental*(hercules+brotherBear+littleMermaid));
+
+var googlePay = 400;
+var amazonPay = 380;
+var facebookPay = 350;
+var googleHours = 6;
+var facebookHours = 10;
+var amazonHours = 4;
+var googleTotal = googlePay * googleHours;
+var facebookTotal = facebookPay * facebookHours;
+var amazonTotal = amazonPay * amazonHours;
+
+var totalPay = googleTotal + amazonTotal + facebookTotal;
+
+var canStudentEnroll = class = !full && (proposedSchedule /== currentSchedule)
+
+var doesOfferApply = itemsBought >= 2 && offerNotExpired = true
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+var passwordLength = function(password) {
+    return password.length >= 5;
+};
+
+var passwordUsernameMatch = function(password){
+    return password.IndexOf(username) === -1;
+};
+
+var usernameNotInPass = password.indexOf(username) === -1;
+var usernameNotInPass1 = !password.includes(username);
+
+var passwordTooLong = function (password){
+    return password.length <=20;
+};
+
+var passwordWhitespace = function (password) {
+    return password.IndexOf(' ') === 0 || password.lastIndexOf(' ');
+};
+
+var noWhiteSpace = username.trim() === username && password.trim() === password;
+
+return prob * prize > pay
